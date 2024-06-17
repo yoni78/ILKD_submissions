8d734d3ec152ae4d9750455484d3aecce2e176ba
jonathan amal <jonathan-amal@spring2024-iit.kdlp.underground.software>
2024-06-17 15:00:35 +0300

    lvl 1
    Completed level 1 - the shell reads lines of input.
    
    Signed-off-by: jonathan amal <jonathan-amal@spring2024-iit.kdlp.underground.software>

--- jonathan-amal/P0/shell.c
+++ jonathan-amal/P0/shell.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define READ_BUFFER_SIZE 1024
+
+
 int main() {
+	char read_buffer[READ_BUFFER_SIZE];
 	char *dir_name = getcwd(NULL, 0);
 
-	printf("%s$ \n", dir_name);
+	do {
+		printf("%s$ ", dir_name);
+	} while(fgets(read_buffer, sizeof(read_buffer), stdin) != NULL);
+
+	printf("\n");
+
+	return 0;
 }
