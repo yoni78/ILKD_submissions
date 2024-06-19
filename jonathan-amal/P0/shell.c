c928d990dceffddead55a9907a3739c35713b83c
jonathan amal <jonathan-amal@spring2024-iit.kdlp.underground.software>
2024-06-19 17:26:18 +0300

    lvl 2
    Completed level 2 - the shell prints "unrecognized command".
    
    Signed-off-by: jonathan amal <jonathan-amal@spring2024-iit.kdlp.underground.software>

--- jonathan-amal/P0/shell.c
+++ jonathan-amal/P0/shell.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define READ_BUFFER_SIZE 1024
 
+
+void parse_input(char buf[READ_BUFFER_SIZE]) {
+	if (strcmp(buf, "\n")  == 0) {
+		return;
+	}
+
+	printf("Unrecognized command\n");
+}
 
 int main() {
 	char read_buffer[READ_BUFFER_SIZE];
 	char *dir_name = getcwd(NULL, 0);
 
-	do {
+	printf("%s$ ", dir_name);
+
+	while(fgets(read_buffer, sizeof(read_buffer), stdin) != NULL) {
+		parse_input(read_buffer);
+
 		printf("%s$ ", dir_name);
-	} while(fgets(read_buffer, sizeof(read_buffer), stdin) != NULL);
+	}
 
 	printf("\n");
 
