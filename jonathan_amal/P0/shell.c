41d0f42edcd3a33fd963a59a713055d20041ae82
Jonathan Amal <jonathan-amal@spring2024-iit.kdlp.underground.software>
2024-06-23 11:39:52 -0300

    lvl 6
    Completed level 6 - the shell manually looks for executables in PATH environment
    variable and runs them in a child process.
    
    Signed-off-by: Jonathan Amal <jonathan-amal@spring2024-iit.kdlp.underground.software>

--- jonathan_amal/P0/shell.c
+++ jonathan_amal/P0/shell.c
@@ -6,46 +6,45 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 const char delimiters[] = " \n\t\v\f\r";
 
-size_t count_tokens(char *str) {
+
+size_t count_tokens(char *str, const char* delim) {
+	char *str_cp = strdup(str);
 	size_t count = 0;
 	char *tok = NULL;
 
-	tok = strtok(str, delimiters);
+	tok = strtok(str_cp, delim);
 
 	while (tok != NULL) {
 		count++;
 
-		tok = strtok(NULL, delimiters);
+		tok = strtok(NULL, delim);
 	}
-
+	free(str_cp);
 	return count;
 }
 
-size_t create_token_list(char *str, char ***tokens) {
-	size_t str_len = strlen(str);
-	char *str_cp = malloc(sizeof(char) * str_len + 1);
-	strcpy(str_cp, str);
-
-	size_t tokens_count = count_tokens(str_cp);
+size_t create_token_list(char *str, char ***tokens, const char* delim) {
+	
+	size_t tokens_count = count_tokens(str, delim);
 
 	*tokens = malloc(sizeof(char *) * tokens_count);
 
 	char *tok = NULL;
 	size_t index = 0;
 
-	tok = strtok(str, delimiters);
+	tok = strtok(str, delim);
 
 	while (tok != NULL) {
 		(*tokens)[index] = tok;
 
 		index++;
-		tok = strtok(NULL, delimiters);
+		tok = strtok(NULL, delim);
 	}
 
-	free(str_cp);
 
 	return tokens_count;
 }
@@ -66,13 +65,71 @@ void run_execv(char **tokens, size_t tokens_count) {
 	}
 }
 
+pid_t fork_and_exec(char **tokens, size_t tokens_count)
+{
+	pid_t pid = fork();
+
+		if (pid == 0) {
+			run_execv(tokens, tokens_count);
+			
+			exit(0);
+
+		} else {
+			waitpid(pid, NULL, 0);
+		}
+	return pid;
+}
+
+char* find_file_path_env(char* file)
+{
+	char* delim = ":";
+	char** dirs;
+	char* env = getenv("PATH");
+	char * env_cp = strdup(env);
+	size_t dirs_count = create_token_list(env_cp,&dirs, delim);
+	
+	if(!dirs || !file)
+		return NULL;
+	
+	char* new_path = NULL;
+	size_t dirs_len;
+	size_t file_len = strlen(file);
+	
+	for(size_t i=0; i<dirs_count;i++)
+	{
+		dirs_len = strlen(dirs[i]);
+		new_path = malloc(dirs_len + file_len + 2);
+
+		strcpy(new_path, dirs[i]);
+
+		new_path[dirs_len] = '/';
+		strcpy(new_path+dirs_len+1, file);
+		new_path[dirs_len+1+file_len] = '\0';
+	 
+		struct stat sb;
+		if(stat(new_path,&sb) == 0 && S_ISREG(sb.st_mode) && (sb.st_mode & S_IXUSR))
+		{
+			break;
+		}
+		else
+		{
+			free(new_path);
+			new_path = NULL;
+		}
+	}
+	
+	free(env_cp);
+	free(dirs);
+	return new_path; 
+}
+
 void parse_input(char *str, char **dir_name) {
 	if (strcmp(str, "\n") == 0) {
 		return;
 	}
 
 	char **tokens;
-	size_t tokens_count = create_token_list(str, &tokens);
+	size_t tokens_count = create_token_list(str, &tokens, delimiters);
 
 	if (tokens_count == 0) {
 		return;
@@ -116,33 +173,34 @@ void parse_input(char *str, char **dir_name) {
 		}
 
     } else if (tokens[0][0] == '.' || tokens[0][0] == '/') {
-		pid_t pid = fork();
-
-		if (pid == 0) {
-			run_execv(tokens, tokens_count);
-
-			exit(0);
-
-		} else {
-			waitpid(pid, NULL, 0);
-		}
+		fork_and_exec(tokens, tokens_count);
 
 	} else {
-		fprintf(stderr, "Unrecognized command: %s\n", tokens[0]);
+		char* path = find_file_path_env(tokens[0]);
+		if(!path){
+			fprintf(stderr, "Unrecognized command: %s\n", tokens[0]);
+		}else{
+			tokens[0] = path;
+			fork_and_exec(tokens, tokens_count);
+			free(path);
+			
+		}
+
 	}
 
 	free(tokens);
 }
 
+
+
+
+
 int main() {
 	char *string = NULL;
 	size_t size = 0;
-	// ssize_t chars_read = 0;
-
 	char *dir_name = getcwd(NULL, 0);
 
 	printf("%s$ ", dir_name);
-
 	while(getline(&string, &size, stdin) != -1) {
 		parse_input(string, &dir_name);
 
