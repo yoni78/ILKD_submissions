303d966aa87067745be49758798fd7632419787c
jonathan amal <jonathan-amal@spring2024-iit.kdlp.underground.software>
2024-06-23 12:45:39 +0300

    lvl 3
    Completed level 3 - the shell prints "unrecognized command: cmd".
    
    Signed-off-by: jonathan amal <jonathan-amal@spring2024-iit.kdlp.underground.software>

--- jonathan_amal/P0/shell.c
+++ jonathan_amal/P0/shell.c
@@ -4,12 +4,63 @@
 #include <stdlib.h>
 
 
+const char delimiters[] = " \n\t\v\f\r";
+
+size_t count_tokens(char *str) {
+	size_t count = 0;
+	char *tok = NULL;
+
+	tok = strtok(str, delimiters);
+
+	while (tok != NULL) {
+		count++;
+
+		tok = strtok(NULL, delimiters);
+	}
+
+	return count;
+}
+
+size_t create_token_list(char *str, char ***tokens) {
+	size_t str_len = strlen(str);
+	char *str_cp = malloc(sizeof(char) * str_len + 1);
+	strcpy(str_cp, str);
+
+	size_t tokens_count = count_tokens(str_cp);
+
+	*tokens = malloc(sizeof(char *) * tokens_count);
+
+	char *tok = NULL;
+	size_t index = 0;
+
+	tok = strtok(str, delimiters);
+
+	while (tok != NULL) {
+		(*tokens)[index] = tok;
+
+		index++;
+		tok = strtok(NULL, delimiters);
+	}
+
+	free(str_cp);
+
+	return tokens_count;
+}
+
 void parse_input(char *str) {
 	if (strcmp(str, "\n") == 0) {
 		return;
 	}
 
-	fprintf(stderr, "Unrecognized command\n");
+	char **tokens;
+	size_t tokens_count = create_token_list(str, &tokens);
+	//create_token_list(str, &tokens);
+	if (tokens_count == 0) {
+		return;
+	}
+	fprintf(stderr, "Unrecognized command: %s\n", tokens[0]);
+
+	free(tokens);
 }
 
 int main() {
@@ -32,6 +83,8 @@ int main() {
 	if (string != NULL) {
 		free(string);
 	}
+
+	free(dir_name);
 
 	return 0;
 }
