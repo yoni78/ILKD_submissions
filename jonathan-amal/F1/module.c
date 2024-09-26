7a56c6b515084e010a9df50d04af531570c0c613
jonathan amal <jonathan-amal@spring2024-iit.kdlp.underground.software>
2024-09-26 16:32:36 +0300

    cleaned up module

--- jonathan-amal/F1/module.c
+++ jonathan-amal/F1/module.c
@@ -6,15 +6,14 @@
 
 #define DEVICE_NAME "cube"
 
-int major_number;
-char cube[6][9];
-struct device *cube_device = NULL;
-DEFINE_MUTEX(cube_mutex);
+static int major_number;
+static char cube_state[6][9];
+static DEFINE_MUTEX(cube_mutex);
 
 #define CUBE_SETUP _IOW('a', 1, unsigned short)
 #define CUBE_IS_SOLVED _IOR('a', 2, unsigned short)
 
-int cube_init(void) {
+static int __init cube_init(void) {
     major_number = register_chrdev(0, DEVICE_NAME, &fops);
 
     if (major_number < 0) {
@@ -22,51 +21,42 @@ int cube_init(void) {
         return major_number;
     }
 
-    cube_device = device_create(cube_class, NULL, MKDEV(major_number, 0), NULL, DEVICE_NAME);
-
-    if (IS_ERR(cube_device)) {
-        unregister_chrdev(major_number, DEVICE_NAME);
-        printk(KERN_ALERT "Failed to create the device\n");
-        return PTR_ERR(cube_device);
-    }
-
     mutex_init(&cube_mutex);
     printk(KERN_INFO "Cube device initialized\n");
     return 0;
 }
 
-void cube_exit(void) {
+static void __exit cube_exit(void) {
     mutex_destroy(&cube_mutex);
-    device_destroy(cube_class, MKDEV(major_number, 0));
     unregister_chrdev(major_number, DEVICE_NAME);
     printk(KERN_INFO "Cube device exited\n");
 }
 
-int dev_open(struct inode *inodep, struct file *filep) {
+static int dev_open(struct inode *inodep, struct file *filep) {
     mutex_lock(&cube_mutex);
     printk(KERN_INFO "Cube device opened\n");
     return 0;
 }
 
-int dev_release(struct inode *inodep, struct file *filep) {
+static int dev_release(struct inode *inodep, struct file *filep) {
     mutex_unlock(&cube_mutex);
     printk(KERN_INFO "Cube device closed\n");
     return 0;
 }
 
-ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
     // TODO: Implement read functionality for cube state
     // TODO: Return -1 and set errno on error
     return 0;
 }
 
-ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
     // TODO: Implement cube rotations here
     // TODO: Return -1 and set errno on invalid moves
     return len;
 }
 
-long dev_ioctl(struct file *filep, unsigned int cmd, unsigned long arg) {
+static long dev_ioctl(struct file *filep, unsigned int cmd, unsigned long arg) {
     switch (cmd) {
         case CUBE_SETUP:
             // TODO: Handle cube setup with random moves
@@ -80,12 +70,12 @@ long dev_ioctl(struct file *filep, unsigned int cmd, unsigned long arg) {
     return 0;
 }
 
-loff_t dev_lseek(struct file *filep, loff_t offset, int whence) {
+static loff_t dev_lseek(struct file *filep, loff_t offset, int whence) {
     // TODO: Implement lseek functionality
     return 0;
 }
 
-struct file_operations fops = {
+static struct file_operations fops = {
     .open = dev_open,
     .read = dev_read,
     .write = dev_write,
